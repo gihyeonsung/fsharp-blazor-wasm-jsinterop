a32bc4c3336094c422809cae3488bd11497eb0f8
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-10-14 17:04:10 +0900

    update example

--- Program.fs
+++ Program.fs
@@ -7,12 +7,42 @@ open Microsoft.JSInterop
 open System
 open System.Net.Http
 
-module Api =
-  [<JSInvokable("add")>]
-  let add x y = task { return x + y }
+(*
+Invoke an instance .NET method
 
-  [<JSInvokable("sub")>]
-  let sub x y = task { return x - y }
+To invoke an instance .NET method from JavaScript (JS):
+
+    Pass the .NET instance by reference to JS by wrapping the instance in a DotNetObjectReference and calling Create on it.
+    Invoke a .NET instance method from JS using invokeMethod or invokeMethodAsync from the passed DotNetObjectReference. The .NET instance can also be passed as an argument when invoking other .NET methods from JS.
+    Dispose of the DotNetObjectReference.
+*)
+
+module Api =
+
+  type Adder () =
+    member val X: int = 0 with get, set
+    member __.Add dx = __.X <- __.X + dx
+
+  [<JSInvokable("createAdder")>]
+  let createAdder () =
+    task {
+      let adder = Adder ()
+      return DotNetObjectReference.Create<Adder>(adder)
+    }
+
+  [<JSInvokable("add")>]
+  let add (adder: DotNetObjectReference<Adder>) dx =
+    task {
+      let adder = adder.Value
+      do adder.Add dx
+      return adder.X
+    }
+
+  [<JSInvokable("add")>]
+  let destroyAdder (adder: DotNetObjectReference<Adder>) =
+    task {
+      do adder.Dispose ()
+    }
 
 module Program =
   [<EntryPoint>]
